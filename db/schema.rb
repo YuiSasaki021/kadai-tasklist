27e39724bae0c97a3d3223f03b74bb87d7ef46da
yui.sasaki <yusasaki@side-stream.co.jp>
2022-12-07 04:27:37 +0000

    4thcommit

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_06_010341) do
+ActiveRecord::Schema.define(version: 2022_12_07_014556) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
 end
