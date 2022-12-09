bae2e21e36cf11ba0294bc50ff112a85b283b11e
yui.sasaki <yusasaki@side-stream.co.jp>
2022-12-09 00:23:17 +0000

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "tasks#index"
+  
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :create]
   resources :tasks
 end
